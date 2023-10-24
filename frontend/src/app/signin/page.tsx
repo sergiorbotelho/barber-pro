3116f7eaee7a9a91d8884e76c4856301c6fd3a7a
sergiorbotelho <sergiorbotelho@gmail.com>
2023-10-23 22:40:46 -0300

    Created page Signin

--- frontend/src/app/signin/page.tsx
+++ frontend/src/app/signin/page.tsx
@@ -3,32 +3,29 @@ import Image from "next/image";
 import logoDom from "../../../public/logoDom.png";
 import Input from "@/components/Input";
 import Link from "next/link";
-const Login = () => {
+
+export default function Sigin() {
   return (
     <div className="container mx-auto flex flex-col justify-center items-center gap-8 text-white h-screen">
-      <Image
-        alt="Logo Dom Barber"
-        src={logoDom}
-        width={0}
-        height={0}
-        className="w-56"
-      />
       <h1 className="font-semibold text-4xl text-center italic">
         Dom Onassis Barber Shop
       </h1>
-      <div className="flex flex-col gap-4 w-full justify-center items-center ">
+      <div className="flex flex-col gap-4 w-full justify-center items-center">
+        <Input label="Nome" type="text" />
         <Input label="Email" type="email" />
+        <Input label="Telefone" type="text" />
+        <Input label="Data Nasc." type="text" />
+
         <Input label="Password" type="password" />
-        <Button title="Entrar" />
+        <Input label="Confirme" type="password" />
+        <Button title="Cadastrar" />
       </div>
       <p className="text-gray-400 text-sm">
-        Não possui uma conta?{" "}
-        <Link href="/signin" className="text-[#eed72c] font-semibold">
-          Cadastre-se
+        Ja possui uma conta?{" "}
+        <Link href="/login" className="text-[#eed72c] font-semibold">
+          Faça o login!
         </Link>
       </p>
     </div>
   );
-};
-
-export default Login;
+}
--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -1,15 +1,39 @@
-import TextFiled from "@mui/material/TextField";
+"use client";
+import { OutlinedInputProps } from "@mui/material/OutlinedInput";
+import TextField, { TextFieldProps } from "@mui/material/TextField";
+import { alpha, styled } from "@mui/material/styles";
 
 interface Props {
   label: string;
+  type: string;
 }
 
-export default function Input({ label }: Props) {
+const RedditTextField = styled((props: TextFieldProps) => (
+  <TextField
+    InputProps={{ disableUnderline: true } as Partial<OutlinedInputProps>}
+    {...props}
+  />
+))(({ theme }) => ({
+  "& .MuiFilledInput-root": {
+    overflow: "hidden",
+    borderRadius: 18,
+    backgroundColor: theme.palette.mode === "light" ? "#F3F6F9" : "#1A2027",
+    transition: theme.transitions.create(["background-color", "box-shadow"]),
+    "&.Mui-focused": {
+      backgroundColor: "transparent",
+      boxShadow: `${alpha(theme.palette.primary.main, 0.25)} 0 0 0 2px`,
+      borderColor: 0,
+    },
+  },
+}));
+
+export default function Input({ label, type }: Props) {
   return (
-    <TextFiled
+    <RedditTextField
+      type={type}
       id="outlined-basic"
       label={label}
-      variant="outlined"
+      variant="filled"
       className="bg-white rounded-xl w-full max-w-xs"
     />
   );
